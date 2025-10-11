6f26e9a8b35c8e0c5ec52209474d679883eed08c
profpyrus <profpyrus@gmail.com>
2025-10-11 19:17:29 +0200

    move distance.isZero back to old place and comment out, as it is never reached

--- src/games/september/shared.ts
+++ src/games/september/shared.ts
@@ -20,8 +20,8 @@ export function calculateTurn(positive: Positive, negatives: Negative[]) {
         const dX = positive.x.neg().add(negative.x);
         const dY = positive.y.neg().add(negative.y);
         const distance = dX.pow(2).plus(dY.pow(2));
-        if (distance.isZero()) continue;
         if (distance.lt(MAGNET_RADIUS_SQ)) return 'hit magnet';
+        // if (distance.isZero()) continue; // never actually true
         totalTurn = totalTurn.plus(Decimal.atan2(dY, dX).sub(positive.angle!).sin().div(distance).mul(GRAVITY));
     }
     return totalTurn;
